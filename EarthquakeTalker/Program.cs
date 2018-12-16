3768ec959918c8bca98121155ce09065673958f0
neurowhai <tlsehdgus321@naver.com>
2018-12-16 00:11:41 -0600

    Revert "Add accelerograph"
    This reverts commit 53f7b2072effd4d737d87e05625f1b3db8f2b6ab.

--- EarthquakeTalker/Program.cs
+++ EarthquakeTalker/Program.cs
@@ -148,8 +148,8 @@ namespace EarthquakeTalker
             { DangerValue = 0.07, DangerWaveTime = 0.01, MinDangerWaveTime = 0.005, IsAccel = false });
             seismographList.Add(new SLinkSeismograph("slinktool.exe", "BHZ", "KG", "TJN", 6.327240e+08 / 100, "대전")
             { DangerValue = 0.03, DangerWaveTime = 0.01, MinDangerWaveTime = 0.005, IsAccel = false });
-            seismographList.Add(new WinstonSeismograph(winstonIp, winstonPort, "00", "ENZ", "AM", "R3E8F", 3.87e+05 * 9.80665 / 100, "포항")
-            { DangerValue = 0.1, DangerWaveTime = 0.6, MinDangerWaveTime = 0.2, IsAccel = true, Endian = true });
+            seismographList.Add(new WinstonSeismograph(winstonIp, winstonPort, "00", "EHZ", "AM", "R3E8F", 3.36e+08 / 100, "포항")
+            { DangerValue = 0.05, DangerWaveTime = 0.6, MinDangerWaveTime = 0.2, IsAccel = false, Endian = true });
 
             /// 지진계를 포함한 메세지 생성자
             List<Worker> workerList = new List<Worker>();
