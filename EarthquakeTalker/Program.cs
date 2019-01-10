71e9a45aa4bc9681501af184ff0fc4600092c371
neurowhai <tlsehdgus321@naver.com>
2019-01-10 04:50:04 -0600

    Decrease danger value of seismographs

--- EarthquakeTalker/Program.cs
+++ EarthquakeTalker/Program.cs
@@ -143,13 +143,13 @@ namespace EarthquakeTalker
             /// 지진계
             List<Seismograph> seismographList = new List<Seismograph>();
             seismographList.Add(new SLinkSeismograph("slinktool.exe", "00BH1", "IU", "INCN", 3.352080e+09 / 100, "인천")
-            { DangerValue = 0.03, DangerWaveTime = 0.01, MinDangerWaveTime = 0.005, IsAccel = false });
+            { DangerValue = 0.01, DangerWaveTime = 0.01, MinDangerWaveTime = 0.005, IsAccel = false });
             seismographList.Add(new SLinkSeismograph("slinktool.exe", "BHZ", "JP", "JTU", 1.000000e+09 / 100, "대마도")
             { DangerValue = 0.07, DangerWaveTime = 0.01, MinDangerWaveTime = 0.005, IsAccel = false });
             seismographList.Add(new SLinkSeismograph("slinktool.exe", "BHZ", "KG", "TJN", 6.327240e+08 / 100, "대전")
-            { DangerValue = 0.03, DangerWaveTime = 0.01, MinDangerWaveTime = 0.005, IsAccel = false });
+            { DangerValue = 0.01, DangerWaveTime = 0.01, MinDangerWaveTime = 0.005, IsAccel = false });
             seismographList.Add(new WinstonSeismograph(winstonIp, winstonPort, "00", "EHZ", "AM", "R3E8F", 3.36e+08 / 100, "포항")
-            { DangerValue = 0.05, DangerWaveTime = 0.6, MinDangerWaveTime = 0.2, IsAccel = false, Endian = true });
+            { DangerValue = 0.04, DangerWaveTime = 0.6, MinDangerWaveTime = 0.2, IsAccel = false, Endian = true });
 
             /// 지진계를 포함한 메세지 생성자
             List<Worker> workerList = new List<Worker>();
