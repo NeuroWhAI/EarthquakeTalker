171967a8c87d6e1b019d07d43b85a17cda338606
NeuroWhAI <tlsehdgus0212@gmail.com>
2019-08-14 20:06:49 +0900

    Remove debugging comments

--- EarthquakeTalker/Program.cs
+++ EarthquakeTalker/Program.cs
@@ -109,8 +109,7 @@ namespace EarthquakeTalker
 #else
             bool needController = File.Exists("EarthquakeTalkerController.exe");
 #endif
-            needController = false;
-
+            
             /// GUI
             Process controller = null;
 
@@ -139,33 +138,33 @@ namespace EarthquakeTalker
             /// 메세지 처리자
             MultipleTalker talker = new MultipleTalker();
 #if DEBUG
-            //talker.AddTalker(new TelegramBot("@neurowhai_test_bot"));
+            talker.AddTalker(new TelegramBot("@neurowhai_test_bot"));
 #else
             talker.AddTalker(new TelegramBot("@neurowhai_earthquake_channel"));
 #endif
-            //talker.AddTalker(new MessageServer(msgServerPort, "messages.dat"));
+            talker.AddTalker(new MessageServer(msgServerPort, "messages.dat"));
 
             /// 지진계
             List<Seismograph> seismographList = new List<Seismograph>();
-            /*seismographList.Add(new SLinkSeismograph("slinktool.exe", "00BH1", "IU", "INCN", 3.352080e+09 / 100, "인천")
+            seismographList.Add(new SLinkSeismograph("slinktool.exe", "00BH1", "IU", "INCN", 3.352080e+09 / 100, "인천")
             { DangerValue = 0.01, DangerWaveTime = 0.01, MinDangerWaveTime = 0.005, IsAccel = false });
             seismographList.Add(new SLinkSeismograph("slinktool.exe", "BHZ", "JP", "JTU", 1.000000e+09 / 100, "대마도")
             { DangerValue = 0.07, DangerWaveTime = 0.01, MinDangerWaveTime = 0.005, IsAccel = false });
             seismographList.Add(new SLinkSeismograph("slinktool.exe", "BHZ", "KG", "TJN", 6.327240e+08 / 100, "대전")
             { DangerValue = 0.01, DangerWaveTime = 0.01, MinDangerWaveTime = 0.005, IsAccel = false });
             seismographList.Add(new WinstonSeismograph(winstonIp, winstonPort, "00", "EHZ", "AM", "R3E8F", 3.36e+08 / 100, "포항")
-            { DangerValue = 0.03, DangerWaveTime = 0.5, MinDangerWaveTime = 0.2, IsAccel = false, Endian = true });*/
+            { DangerValue = 0.03, DangerWaveTime = 0.5, MinDangerWaveTime = 0.2, IsAccel = false, Endian = true });
 
             /// 지진계를 포함한 메세지 생성자
             List<Worker> workerList = new List<Worker>();
             //workerList.AddRange(seismographList);
             workerList.Add(new UserWatcher("KMA_earthquake", new KMAEarthquakeFormatter()));
-            /*workerList.Add(new KmaHome());
+            workerList.Add(new KmaHome());
             workerList.Add(new KmaMicro());
             workerList.Add(new IssueWatcher("지진", "지진+-동공+-일본+-원전+-http+-카메라+-ㅋㅋㅋ+-캠",
                 triggerTime: TimeSpan.FromSeconds(30), maxStatusCount: 20, maxTextLength: 32));
             workerList.Add(new NecisEarlyWarning());
-            workerList.Add(new KmaNews());*/
+            workerList.Add(new KmaNews());
 
             int sensorIndex = 0;
             foreach (var sensor in seismographList)
